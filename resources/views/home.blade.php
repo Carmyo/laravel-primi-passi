ba4a6acf9b4e50189e6b7c1787ba0713ddce5e84
Carmyo <carmelodicristina@gmail.com>
2022-11-02 00:05:54 +0100

    set for each

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,28 @@
 <h1> Hello World </h1>
+<?php
+$data = [
+    [
+        'title'=> 'comic1',
+        'price'=> 11,
+
+    ],
+    [
+        'title'=> 'comic2',
+        'price'=> 10,
+
+    ],
+    [
+        'title'=> 'comic3',
+        'price'=> 13,
+
+
+    ],
+]
+
+?>
+<?php
+foreach ($data as $item)
+
+<p>{{$item['title']}}</p>
+
+?>
